209e67219e89703ad13986cf047869257fcef263
Yago Gomes <yagogomesc@gmail.com>
2020-12-01 19:36:23 -0300

    Add função de mascara para formatação do numero de telefone

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -4,3 +4,17 @@ $('#archive').on('change',function(){
 
     $(this).next('.custom-file-label').html(archiveName);
 })
+
+//Mascara para formatação do numero de telefone
+$(document).ready(function() {
+    var phone = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+    },
+    phoneOptions = {
+        onKeyPress: function(val, e, field, options) {
+            field.mask(phone.apply({}, arguments), options);
+        }
+    };
+
+    $('#phone').mask(phone, phoneOptions);
+});
