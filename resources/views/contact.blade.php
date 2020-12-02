8417bca462e54396e18ccd8baa989e3e1c4a84dc
Yago Gomes <yagogomesc@gmail.com>
2020-12-02 19:09:42 -0300

    Ajustado formulario, adicionado value old para permanecer valor ja inserido em caso de erro

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,13 +13,15 @@
         <div class="container">
             <div class="page-contact col-sm-8 offset-sm-2">
                 @if ($errors->any())
-                    <div class="alert alert-danger">   
+                    <div class="alert alert-danger mt-2">   
                         <ul>
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
                             @endforeach
                         </ul>
                     </div>
+                @elseif (session('success'))
+                    <div class="alert alert-success mt-2">{{session('success')}}</div>
                 @endif
                 <div class="card mt-5">
                     <div class="card-header">
@@ -28,26 +30,26 @@
                         </h3>
                     </div>
                     <div class="card-body">
-                    <form enctype="multipart/form-data" action="{{route('contact.store')}}" method="POST">
+                    <form action="{{route('contact.store')}}" enctype="multipart/form-data" method="POST">
                         @csrf
                         <div class="form-group">
                             <label for="name">Nome</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Nome">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Nome" value="{{old('name')}}">
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="{{old('email')}}">
                         </div>
                         <div class="form-group">
                             <label for="phone">Telefone</label>
-                            <input type="text" class="form-control" id="phone" name="phone" placeholder="(xx) xxxx-xxxx">
+                            <input type="text" class="form-control" id="phone" name="phone" placeholder="(xx) xxxx-xxxx" value="{{old('phone')}}">
                         </div>
                         <div class="form-group">
                             <label for="message">Mensagem</label>
-                            <textarea class="form-control" id="message" name="message" rows="3"> </textarea>
+                            <textarea class="form-control" id="message" name="message" rows="3">{{old('message')}}</textarea>
                         </div>
                         <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="archive" required>
+                            <input type="file" class="custom-file-input" id="archive" name="archive" required>
                             <label class="custom-file-label" for="archive">Anexar arquivo...</label>
                         </div>
                     </div>
