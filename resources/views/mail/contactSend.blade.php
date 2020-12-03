416b6129739aca8c01e595d3b7d10d84fd953366
Yago Gomes <yagogomesc@gmail.com>
2020-12-03 15:07:39 -0300

    Corpo do email para ser enviado

---
+++ resources/views/mail/contactSend.blade.php
@@ -0,0 +1,13 @@
+<h3>Email de registro de contato</h3>
+
+Foi realizado um contato com os seguintes dados:
+
+<p>Nome: {{$name}}</p>
+
+<p>Email: {{$email}}</p>
+
+<p>Telefone: {{$phone}}</p>
+
+<p>Mensagem: {{$bodyMessage}}</p>
+
+<p>Arquivo armazenado: <a href="{{url($archive)}}">{{$archive}}</a></p>
