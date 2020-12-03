d8151111cd86e557eb52cf5063f421c2da70cb98
Yago Gomes <yagogomesc@gmail.com>
2020-12-03 14:38:25 -0300

    Cadastrado observer no serviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Contact;
+use App\Observers\ContactObserver;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Contact::observe(ContactObserver::class);
     }
 }
