94bdd6ea6f3e2ac0704c38e179027e5d82cf0a86
Yago Gomes <yagogomesc@gmail.com>
2020-12-03 20:01:44 -0300

    Ajustado teste de contato
    Teste verifica recebimento dos dados do formulario no sistema e também verifica a existencia do arquivo gerado

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -4,19 +4,30 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ContactTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Teste de envio do formulário de contato.
      *
      * @return void
      */
-    public function testExample()
+    public function testSendContact()
     {
-        $response = $this->get('/');
+        Storage::fake('local');
+        $documentoTest = UploadedFile::fake()->create('docTest.pdf', 400);
+        
+        $this->post('/', [
+            'name' => 'Yago',
+            'email' => 'yago@gmail.com',
+            'phone' => '(22) 99970-0516',
+            'message' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'archive' => $documentoTest,
+        ])->assertSessionHasNoErrors();
 
-        $response->assertStatus(200);
+        Storage::disk('local')->assertExists("contacts/archives/{$documentoTest->name}");
     }
 }
