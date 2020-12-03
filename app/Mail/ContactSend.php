c72f88d9bf0d2d7d72f93c26299e54f26fba4069
Yago Gomes <yagogomesc@gmail.com>
2020-12-03 14:38:11 -0300

    Classe para envio de email de contato

---
+++ app/Mail/ContactSend.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use App\Contact;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactSend extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Contato')
+                    ->view('mail.contactSend');
+    }
+}
